5ee17387d89a2090a28ddffc6ae309f8b42a24ba
sahilkumar vaghasiya <116561090+sahil3155@users.noreply.github.com>
2026-05-02 16:09:16 +0530

    Update ALLOWED_HOSTS to allow all hosts in settings.py

--- core/config/settings.py
+++ core/config/settings.py
@@ -39,7 +39,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 DEBUG = os.getenv("DEBUG", "False").lower() == "true"
 
 # ALLOWED_HOSTS = ["127.0.0.1", "localhost", "10.0.2.2", "*", "c847-2401-4900-790b-280b-9c9-b8d3-c460-a709.ngrok-free.app", "your-app.up.railway.app"]
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS")
+ALLOWED_HOSTS = ["*"]
 # Application definition
 
 INSTALLED_APPS = [
