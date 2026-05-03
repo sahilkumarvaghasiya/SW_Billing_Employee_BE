27e01f0e180ecd97d1761f78d792ecee8a125e68
Dev Vaghasiya <Devvaghasiya176@gmail.com>
2026-05-03 13:18:23 +0530

    Update setting for db

--- core/config/settings.py
+++ core/config/settings.py
@@ -126,7 +126,11 @@ WSGI_APPLICATION = 'core.config.wsgi.application'
 
 
 DATABASES = {
-    "default": dj_database_url.config(conn_max_age=600)
+    'default': dj_database_url.config(
+        default=os.environ.get("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
