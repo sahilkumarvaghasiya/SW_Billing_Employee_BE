c1a81830db64cb78b0496da6a266cadea43af21b
Dev Vaghasiya <Devvaghasiya176@gmail.com>
2026-05-17 19:51:28 +0530

    Add null field for name

--- core/apps/products/models.py
+++ core/apps/products/models.py
@@ -5,7 +5,7 @@ from django.conf import settings
 
 
 class NameMixin(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, blank=True, null=True)
     shop = models.ForeignKey(Shop, on_delete=models.CASCADE, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
