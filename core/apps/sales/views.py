62b6438bf033dd8ff1d487571cb74705a0d81fbf
Dev Vaghasiya <Devvaghasiya176@gmail.com>
2026-05-31 19:45:04 +0530

    Remove log

--- core/apps/sales/views.py
+++ core/apps/sales/views.py
@@ -241,7 +241,6 @@ class BillCreateViewSet(viewsets.ModelViewSet):
 
     @transaction.atomic
     def create(self, request, *args, **kwargs):
-        print("Bill creation started",request.data)
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         validated_data = serializer.validated_data
